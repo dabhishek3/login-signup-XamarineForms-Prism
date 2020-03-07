7638c1750b925ebd9d978e436f279ae2568a5ad1
dabhishek3 <32655394+dabhishek3@users.noreply.github.com>
2020-03-07 14:50:17 +0530

    Add files via upload

---
+++ SQLite_Android.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Logintest.Droid;
+using Logintest.Models;
+using SQLite;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(SQLite_Android))]
+namespace Logintest.Droid
+{
+    public class SQLite_Android : ISQLiteInterface
+    {
+        SQLiteConnection conn;
+        public SQLiteConnection GetConnection()
+        {
+            var dbName = "Myapp.db";
+            var dbPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.ApplicationData);
+            var path = Path.Combine(dbPath, dbName);
+            var conn = new SQLiteConnection(path);
+            return conn;
+        }
+        
+    }
+}
